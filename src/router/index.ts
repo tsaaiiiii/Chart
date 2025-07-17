15892192c16b06e7cd32c0beb71c2d04efeee65d
Windy <windy.tsai@accuhit.net>
2025-07-17 20:41:58 +0800

    feat: 調整路由設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -24,10 +24,10 @@ const router = createRouter({
       component: () => import('@/views/LineChart.vue'),
     },
     {
-      path:'/table',
-      name:'Table',
-      component:()=>import('@/views/Table.vue')
-    }
+      path: '/table',
+      name: 'Table',
+      component: () => import('@/views/Table.vue'),
+    },
   ],
 })
 
